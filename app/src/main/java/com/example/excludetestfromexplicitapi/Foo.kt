52df027a267e11d37f69052af9c2d164bbccfbc7
Chao Zhang <cazhang@linkedin.com>
2020-12-11 00:04:52 -0800

    First commit

---
+++ app/src/main/java/com/example/excludetestfromexplicitapi/Foo.kt
@@ -0,0 +1,5 @@
+package com.example.excludetestfromexplicitapi
+
+public class Foo {
+    public fun bar(a: Int, b: Int): Int = a * b
+}
---
+++ app/src/test/java/com/example/excludetestfromexplicitapi/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package com.example.excludetestfromexplicitapi
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, Foo().bar(2, 2))
+    }
+}
